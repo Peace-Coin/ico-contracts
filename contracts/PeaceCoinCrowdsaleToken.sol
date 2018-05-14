d21062394d095aa58f292d3e286e9e9396d88cc1
Takeo Hiramatsu <takeo@thebind.io>
2018-05-14 16:21:33 +0900

    20180514 update commit

--- contracts/PeaceCoinCrowdsaleToken.sol
+++ contracts/PeaceCoinCrowdsaleToken.sol
@@ -4,14 +4,14 @@ import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 
 /**
- * @title SampleCrowdsaleToken
+ * @title PeaceCoinCrowdsaleToken
  * @dev Very simple ERC20 Token that can be minted.
  * It is meant to be used in a crowdsale contract.
  */
 contract PeaceCoinCrowdsaleToken is MintableToken {
 
-    string public constant name = "Peace Coin Crowdsale Token"; // solium-disable-line uppercase
-    string public constant symbol = "PCE-Token"; // solium-disable-line uppercase
-    uint8 public constant decimals = 18; // solium-disable-line uppercase
+    string public constant name = "Peace Coin Crowdsale Token";
+    string public constant symbol = "PCE";
+    uint8 public constant decimals = 18;
 
 }
--- contracts/PeaceCoinSavingsWallet.sol
+++ contracts/PeaceCoinSavingsWallet.sol
@@ -3,14 +3,7 @@ pragma solidity ^0.4.21;
 import "zeppelin-solidity/contracts/ownership/Heritable.sol";
 
 /**
- * @title SimpleSavingsWallet
- * @dev Simplest form of savings wallet whose ownership can be claimed by a heir
- * if owner dies.
- * In this example, we take a very simple savings wallet providing two operations
- * (to send and receive funds) and extend its capabilities by making it Heritable.
- * The account that creates the contract is set as owner, who has the authority to
- * choose an heir account. Heir account can reclaim the contract ownership in the
- * case that the owner dies.
+ * @title PeaceCoinSavingsWallet
  */
 contract PeaceCoinSavingsWallet is Heritable {
 
