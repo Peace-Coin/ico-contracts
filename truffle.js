8813a0475be1fa74ce70c4d00a4fe5a4cea291ae
Takeo Hiramatsu <takeo@thebind.io>
2018-06-04 11:03:36 +0900

    update

--- truffle.js
+++ truffle.js
@@ -4,39 +4,17 @@ require('babel-polyfill');
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
-// const infuraProvider = new HDWalletProvider(
-//   'year fabric curtain garbage nasty tag quit idea cotton patient pet bone',
-//   'https://rinkeby.infura.io/AUMpzXam2BRfzMQULuUe'
-// );
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  // networks: {
-  //           development: {
-  //           host: "localhost",
-  //           port: 8545,
-  //           network_id: "*" // Match any network id
-  //       }
-  //   }
   networks: {
-    rinkebyInfura: {
-      provider: function() {
-        return new HDWalletProvider(
-          'year fabric curtain garbage nasty tag quit idea cotton patient pet bone',
-          'https://rinkeby.infura.io/AUMpzXam2BRfzMQULuUe'
-        );
-      },
-      network_id: 4,
-      gas: 4698712
-    },
     ganache: {
       host: 'localhost',
       port: 7545,
       network_id: '*' // Match any network id
     },
-    rinkeby:{
-      host: "localhost",
+    rinkeby: {
+      host: 'localhost',
       port: 8545,
       network_id: 4,
       gas: 4700000
